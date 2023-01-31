bb54aaa628e3455035dd7a89313df148462fafd9
Muhammed <mohammedmarenah7@gmail.com>
2023-01-30 23:02:07 -0500

    changed the texts

--- weather.java
+++ weather.java
@@ -4,17 +4,17 @@ public class weather {
 
 public static void main(String[] args){
     Scanner weather = new Scanner(System.in);
-    System.out.println("What is the weather today?");
+    System.out.println("What is the temperature outside today?");
 
     int temperature = weather.nextInt();
     if (temperature >= 50 )
     {
-        System.out.println("It is hot outside!");
+        System.out.println("It is hot outside, be sure to wear wear a beach hat!");
 
     }
     else if (temperature < 50)
     {
-        System.out.println("It is cold outside!");
+        System.out.println("It is cold outside, be sure to wear a jacket!");
     }
 
 
